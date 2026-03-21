e39cff05a923802ce0b24baa204b2fe9f0d95bd0
joseph-munoz-03 <jmunozp37@gmail.com>
2026-03-21 09:35:04 -0500

    arreglos dentro del host (gracias a dios) (ya se me habian acabado los nombres)

--- SGIEVpy/settings.py
+++ SGIEVpy/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-ldqnxfs*nh0$*6g^%p^)^pg_#t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,*.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,ez-s171.onrender.com,*.onrender.com').split(',')
 
 
 # Application definition

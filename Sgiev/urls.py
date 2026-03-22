54a75c232541a3f37f722b54456b10907453c11a
joseph-munoz-03 <jmunozp37@gmail.com>
2026-03-22 07:02:09 -0500

    lo mismo de antes pero más bonito xd

--- Sgiev/urls.py
+++ Sgiev/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
-    path('login', views.login, name='login'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
     path('admin', views.admin, name='admin'),
 
     #CATEGORIA
